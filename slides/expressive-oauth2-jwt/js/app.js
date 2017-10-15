0973f40c2c7fc5bea4055817d4c204e659c4e332
Vinícius Campitelli <vcampitelli@curseduca.com>
2017-10-14 22:15:05 -0300

    Atualização dos slides da palestra de Expressive

--- slides/expressive-oauth2-jwt/js/app.js
+++ slides/expressive-oauth2-jwt/js/app.js
@@ -1,3 +1,10 @@
+Reveal.initialize({
+    dependencies: [
+        { src: 'js/prism.js', async: true }
+    ],
+    slideNumber: true,
+    transition: 'convex'
+});
 Reveal.addEventListener('fragmentshown', function(event) {
     if (event.fragment.classList.contains('fragment-jwt-code')) {
         event.fragment.style.marginLeft = -event.fragment.clientWidth / 2 + 'px';
