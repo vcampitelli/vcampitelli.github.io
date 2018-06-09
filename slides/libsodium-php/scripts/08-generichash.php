9337fc5a6ecc2b3121b096f8e445462dd0c0b159
Vinícius Campitelli <vcampitelli@curseduca.com>
2018-06-09 10:21:23 -0300

    Trazendo códigos da libsodium nos slides

--- slides/libsodium-php/scripts/08-generichash.php
+++ slides/libsodium-php/scripts/08-generichash.php
@@ -10,10 +10,10 @@ $secretKey = trim(file_get_contents('08-generichash.key'));
 
 // Alternativa à md5/sha1
 $hash = sodium_crypto_generichash($data);
-echo 'Hash simples (alternativa à md5/sha1): ';
+echo 'Hash simples (alternativa a md5/sha1): ' . PHP_EOL;
 var_dump(sodium_bin2hex($hash));
 
 // MAC
-echo 'HMAC: ';
+echo 'HMAC: ' . PHP_EOL;
 $blake2mac = sodium_crypto_generichash($data, $secretKey);
 var_dump(sodium_bin2hex($blake2mac));
