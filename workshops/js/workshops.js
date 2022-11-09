7d8bf269ecd7b06b5801cf4d86891828bf924005
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2022-11-09 17:47:27 -0300

    :lipstick: Adicionando botão flutuante do WhatsApp

--- workshops/js/workshops.js
+++ workshops/js/workshops.js
@@ -1,6 +1,5 @@
-const $links = document.getElementById('contact').children;
-$links[0].querySelector('span > span').innerHTML = '&#040;&#049;&#049;&#041;&#032;&#057;&#057;&#049;&#054;&#054;&#045;&#057;&#049;&#057;&#048;';
-$links[1].querySelector('span > span').innerHTML = '&#101;&#117;&#064;&#118;&#105;&#110;&#105;&#099;&#105;&#117;&#115;&#099;&#097;&#109;&#112;&#105;&#116;&#101;&#108;&#108;&#105;&#046;&#099;&#111;&#109;';
+document.getElementById('footer-1').innerHTML = '&#040;&#049;&#049;&#041;&#032;&#057;&#057;&#049;&#054;&#054;&#045;&#057;&#049;&#057;&#048;';
+document.getElementById('footer-2').innerHTML = '&#101;&#117;&#064;&#118;&#105;&#110;&#105;&#099;&#105;&#117;&#115;&#099;&#097;&#109;&#112;&#105;&#116;&#101;&#108;&#108;&#105;&#046;&#099;&#111;&#109;';
 
 const $content = document.getElementById('content');
 document.querySelector('#header-intro > a').onclick = function (e) {
