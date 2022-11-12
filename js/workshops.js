b5ff209fdfd3b59356e1bef06e8339daf2af6f58
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2022-11-12 12:10:40 -0300

    :building_construction: Alteração da página principal para a de workshops

--- js/workshops.js
+++ js/workshops.js
@@ -1,8 +1,8 @@
 document.getElementById('footer-1').innerHTML = '&#040;&#049;&#049;&#041;&#032;&#057;&#057;&#049;&#054;&#054;&#045;&#057;&#049;&#057;&#048;';
 document.getElementById('footer-2').innerHTML = '&#101;&#117;&#064;&#118;&#105;&#110;&#105;&#099;&#105;&#117;&#115;&#099;&#097;&#109;&#112;&#105;&#116;&#101;&#108;&#108;&#105;&#046;&#099;&#111;&#109;';
 
-const $content = document.getElementById('content');
-document.querySelector('#header-intro > a').onclick = function (e) {
+const $content = document.getElementById('sobre');
+document.querySelector('#header-intro > .d-flex > a').onclick = function (e) {
     if (!e.target.attributes.href) {
         return;
     }
